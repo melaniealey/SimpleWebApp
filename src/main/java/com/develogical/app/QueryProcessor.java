d7dc7ea90a92a5e4e0a9cdb78c21a10c5d1ee72f
Nick Mayne <jetski5822@gmail.com>
2018-07-02 14:10:00 +0100

    Add Monet

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("monet")) {
+            return "I like" +
+                    "" +
+                    " paintings.";
+        }
         return "";
     }
 }
